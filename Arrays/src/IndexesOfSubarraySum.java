339db13cb2ef2ca80ee5cedec8529263226cde60
Akshay Patil <38535200+akshayPatil01@users.noreply.github.com>
2024-06-20 12:20:35 +0530

    Java solution for Indexes of subarray sum (#4)
    * ProblemSolving #3 : Java solution for Indexes of Subarray Sum

---
+++ Arrays/src/IndexesOfSubarraySum.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexesOfSubarraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 7, 5};
+        int n = arr.length;
+        int s = 12;
+
+        ArrayList<Integer> sumList = subarraySum(arr, n, s);
+
+        System.out.println("Indexes of Subarray sum: " + sumList);
+    }
+
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        int start = 0;
+        int sum = 0;
+
+        for (int end = 0; end < n; end++) {
+            sum += arr[end];
+
+            while (sum > s && start < end) {
+                sum -= arr[start];
+                start++;
+            }
+
+            if (sum == s)
+                return new ArrayList<>(List.of(start + 1, end + 1));
+        }
+
+        return new ArrayList<>(List.of(-1));
+    }
+}
