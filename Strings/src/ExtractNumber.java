c26ef8b10a6bca16c4b5e7e5257acd848ae403a7
Akshay Patil <38535200+akshayPatil01@users.noreply.github.com>
2024-06-27 17:19:47 +0530

    Extract number from string (#20)
    * ProblemSolving #19 : Extract the number from String - Java problem resolved.

---
+++ Strings/src/ExtractNumber.java
@@ -0,0 +1,45 @@
+public class ExtractNumber {
+    public static void main(String[] args) {
+        ExtractNumber e = new ExtractNumber();
+        String sentence = "This is alpha 5057 and 97";
+        System.out.println("Max Extracted Number: " + e.extractNumber(sentence));
+    }
+
+    long extractNumber(String sentence) {
+        // code here
+        String[] words = sentence.split(" ");
+
+        long maxNum = -1;
+
+        for (String word : words) {
+            if (isNumber(word)) {
+                long curNum = Long.parseLong(word);
+                if (!containsNine(curNum) && curNum > maxNum) {
+                    maxNum = curNum;
+                }
+            }
+        }
+
+        return maxNum;
+    }
+
+    private boolean containsNine(long num) {
+        while (num > 0) {
+            long digit = num % 10;
+            if (digit == 9)
+                return true;
+            num /= 10;
+        }
+
+        return false;
+    }
+
+    private boolean isNumber(String word) {
+        for (char c : word.toCharArray()) {
+            if (!Character.isDigit(c))
+                return false;
+        }
+
+        return true;
+    }
+}
