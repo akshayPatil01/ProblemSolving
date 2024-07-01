ec256415756f5d88cf006339e61d9dc4f5ed35c6
akshayPatil01 <akdpatil1@gmail.com>
2024-07-01 14:01:51 +0530

    ProblemSolving #34 : Reverse a Linked List - Java problem resolved.

---
+++ LinkedList/src/ReverseLL.java
@@ -0,0 +1,35 @@
+import Util.LLUtilMethods;
+import Util.Node;
+
+import java.util.Arrays;
+
+public class ReverseLL {
+    public static void main(String[] args) {
+        Node head = LLUtilMethods.createLL(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.print("Original LL: ");
+        LLUtilMethods.printLL(head);
+
+        ReverseLL r = new ReverseLL();
+        head = r.reverseList(head);
+
+        System.out.print("Reversed LL: ");
+        LLUtilMethods.printLL(head);
+    }
+
+    Node reverseList(Node head) {
+        // code here
+        Node next = null;
+        Node current = head;
+        Node prev = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+        return head;
+    }
+}
